f488e2b5a8dbcc2625cf193bdfc9d3083fd116de
Ko-Kwan Mongkholtham <kokwan.m@gmail.com>
2023-12-28 15:03:52 +0700

    feat(q3): add q3

--- lab_05/q3/complete.py
+++ lab_05/q3/complete.py
@@ -1,83 +1,91 @@
 import turtle
 
-class Disk(object):
-    def __init__(self, name="", xpos=0, ypos=0, height=20, width=40):
-        self.dname = name
-        self.dxpos = xpos
-        self.dypos = ypos
-        self.dheight = height
-        self.dwidth = width
+class Disk:
+    def __init__(self, name, xpos, ypos, height, width):
+        self.name = name
+        self.xpos = xpos
+        self.ypos = ypos
+        self.height = height
+        self.width = width
 
     def showdisk(self):
-        turtle.goto(self.dxpos, self.dypos)
-        turtle.setheading(0)  
+        turtle.penup()
+        turtle.goto(self.xpos - self.width/2, self.ypos)
         turtle.pendown()
-        turtle.forward(self.dwidth)
-        turtle.left(90)
-        turtle.forward(self.dheight)
-        turtle.left(90)
-        turtle.forward(self.dwidth)
-        turtle.left(90)
-        turtle.forward(self.dheight)
-        turtle.left(90)
+        for _ in range(2):
+            turtle.forward(self.width)
+            turtle.left(90)
+            turtle.forward(self.height)
+            turtle.left(90)
         turtle.penup()
 
     def newpos(self, xpos, ypos):
-        self.dxpos = xpos
-        self.dypos = ypos
+        self.xpos = xpos
+        self.ypos = ypos
 
     def cleardisk(self):
-        turtle.goto(self.dxpos, self.dypos)
-        turtle.setheading(0)  
+        turtle.penup()
+        turtle.goto(self.xpos - self.width/2, self.ypos)
+        turtle.pendown()
+        turtle.color("white")
+        for _ in range(2):
+            turtle.forward(self.width)
+            turtle.left(90)
+            turtle.forward(self.height)
+            turtle.left(90)
+        turtle.color("black")
+        turtle.penup()
 
-class Pole(object):
-    def __init__(self, name="", xpos=0, ypos=0, thick=10, length=100):
-        self.pname = name
+class Pole:
+    def __init__(self, name, xpos, ypos, thick, length):
+        self.name = name
         self.stack = []
-        self.toppos = 0
-        self.pxpos = xpos
-        self.pypos = ypos
-        self.pthick = thick
-        self.plength = length
+        self.toppos = ypos
+        self.xpos = xpos
+        self.ypos = ypos
+        self.thick = thick
+        self.length = length
 
     def showpole(self):
-        turtle.goto(self.pxpos, self.pypos)
-        turtle.setheading(0)  # East
+        turtle.penup()
+        turtle.goto(self.xpos - self.thick/2, self.ypos)
         turtle.pendown()
-        turtle.forward(self.pthick)
+        turtle.forward(self.thick)
         turtle.left(90)
-        turtle.forward(self.plength)
+        turtle.forward(self.length)
         turtle.left(90)
-        turtle.forward(self.pthick)
+        turtle.forward(self.thick)
         turtle.left(90)
-        turtle.forward(self.plength)
+        turtle.forward(self.length)
         turtle.left(90)
         turtle.penup()
 
     def pushdisk(self, disk):
-        disk.newpos(self.pxpos, self.toppos)
+        disk.newpos(self.xpos, self.toppos)
         self.stack.append(disk)
-        self.toppos += disk.dheight
+        self.toppos += disk.height
         disk.showdisk()
 
     def popdisk(self):
         if self.stack:
             disk = self.stack.pop()
-            self.toppos -= disk.dheight
+            disk.cleardisk()
+            self.toppos -= disk.height
             return disk
         return None
 
-class Hanoi(object):
+class Hanoi:
     def __init__(self, n=3, start="A", workspace="B", destination="C"):
-        self.start = Pole(start, 0, 0)
-        self.workspace = Pole(workspace, 150, 0)
-        self.destination = Pole(destination, 300, 0)
+        self.start = Pole(start, 0, -100, 20, 300)
+        self.workspace = Pole(workspace, 150, -100, 20, 300)
+        self.destination = Pole(destination, 300, -100, 20, 300)
         self.start.showpole()
         self.workspace.showpole()
         self.destination.showpole()
 
         for i in range(n):
-            self.start.pushdisk(Disk("d" + str(i), 0, i * 20, 20, (n - i) * 30))
+            disk = Disk("d" + str(i), 0, -100 + i * 20, 20, 40 + (n-i-1) * 20)
+            self.start.pushdisk(disk)
 
     def movedisk(self, start, destination):
         disk = start.popdisk()
@@ -94,7 +102,11 @@ class Hanoi(object):
     def solve(self):
         self.movetower(3, self.start, self.destination, self.workspace)
 
-turtle.speed(7) 
-h = Hanoi()
+turtle.speed(0) 
+turtle.bgcolor("white")
+turtle.hideturtle()
+
+h = Hanoi(3)
 h.solve()
+
 turtle.done()
