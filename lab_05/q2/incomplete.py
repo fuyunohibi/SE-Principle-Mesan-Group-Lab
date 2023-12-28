1dbd48f4ddb6853ea7b1bdf0631f78be61aa07d5
Ko-Kwan Mongkholtham <kokwan.m@gmail.com>
2023-12-28 14:02:26 +0700

    feat(q2)

--- lab_05/q2/incomplete.py
+++ lab_05/q2/incomplete.py
@@ -24,14 +24,26 @@ class Walk( Transportation ):
 
    
 # main program
+class Jet(Transportation):
+    def __init__(self, start, end, distance, speed):
+        Transportation.__init__(self, start, end, distance)
+        self.speed = speed  # Speed of the jet in km/h
+
+    def find_cost(self):
+        base_rate = 50 
+        speed_factor = 100 
+        cost_per_km = max(base_rate - self.speed / speed_factor, base_rate / 2)
+        return self.distance * cost_per_km
+
+
+
+trip = [ 
+  Walk("KMITL","KMITL SCB Bank", 0.6),
+  Jet("KMITL SCB Bank", "KMITL 7-11", 10, 900),
+]
 
 travel_cost = 0
-
-#trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
-         #Taxi("KMITL SCB Bank","Ladkrabang Station",5),
-         #Train("Ladkrabang Station","Payathai Station",40,6),
-         #Taxi("Payathai Station","The British Council",3) ]
-
 for travel in trip:
    travel_cost += travel.find_cost()
-print travel_cost
+
+print(travel_cost)
