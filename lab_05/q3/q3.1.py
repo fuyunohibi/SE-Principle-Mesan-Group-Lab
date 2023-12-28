f4d193c9b6f8ffefcd16a17a6b65e5a862c7e971
pcr9x <petchppw@gmail.com>
2023-12-28 14:23:12 +0700

    feat(q3): add q1 petch

--- lab_05/q3/q3.1.py
+++ lab_05/q3/q3.1.py
@@ -45,3 +45,14 @@ class Disk(object):
         turtle.setheading(0) 
         
 
+if __name__ == "__main__":
+    d1 = Disk("d1", 0, 0)
+    d1.showdisk()
+    d1.newpos(0, 100)
+    d1.cleardisk()
+    d1.newpos(0, 0)
+    d2 = Disk("d2", 0, 0, 10, 50)
+    d2.showdisk()
+    d3 = Disk("d3", 0, 0, 5, 30)
+    d3.showdisk()
+    turtle.mainloop()
