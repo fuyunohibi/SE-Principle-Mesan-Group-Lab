ff45b7cc0cac005edee899a01c8b48b7cae65cc1
Ko-Kwan Mongkholtham <kokwan.m@gmail.com>
2023-12-28 13:41:20 +0700

    first commit

--- lab_05/q1_capt.py
+++ lab_05/q1_capt.py
@@ -25,12 +25,27 @@ class Simple_drawing_window(QWidget):
 
         p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
         p.end()
+    
+  
+class Simple_drawing_window1(Simple_drawing_window):
+    def paintEvent(self, e):
+        super().paintEvent(e)
+        p = QPainter()
+        p.begin(self)
 
+    
+        p.setPen(QColor(255, 255, 0))
+        p.setBrush(QColor(255, 255, 0))
+        p.drawPolygon([QPoint(350, 100), QPoint(370, 150), QPoint(400, 150), QPoint(375, 175), QPoint(390, 220), QPoint(350, 190), QPoint(310, 220), QPoint(325, 175), QPoint(300, 150), QPoint(330, 150),])
+        
+        p.end()
+
+        
 def main():
     app = QApplication(sys.argv)
-
-    w = Simple_drawing_window()
-    w.show()
+    
+    window_1 = Simple_drawing_window1()
+    window_1.show()
 
     return app.exec_()
 
